032570bbb185d38bb0008f104db5e324761ea2ba
Lim Chae A <imchae4@gmail.com>
2025-11-08 11:38:47 +0900

    서브 리스트 jsx 수정

--- src/components/Depth1.jsx
+++ src/components/Depth1.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import SubmenuList from './SubMenuList';
 
 const Depth1 = () => {
     return (
         <div className="depth1">
-            <div className="depth1_left">
-                <SubmenuList />
-            </div>
+            <div className="depth1_left"></div>
             <div className="depth1_right"></div>
         </div>
     );
