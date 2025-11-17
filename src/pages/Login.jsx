a9af4432e83e7304c6acc7a5470f69297574c257
Lim Chae A <imchae4@gmail.com>
2025-11-17 15:52:40 +0900

    로그인jsx 수정

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,33 +47,6 @@ const Login = () => {
                         <Link to="/nonmember">비회원 주문조회</Link>
                     </p>
                 </div>
-                {/* <div className="crocsclub_wrap">
-                    <Title subTitle="CrocsClub Benefit" />
-                    <ul className="benefit_list">
-                        <li>
-                            <span>
-                                <img src="./images/benefit_img_01.svg" alt="benefit" />
-                            </span>
-                            첫 주문 <br />
-                            추가 15% 할인
-                        </li>
-                        <li>
-                            <span>
-                                <img src="./images/benefit_img_02.svg" alt="benefit" />
-                            </span>
-                            온라인 단독 <br />
-                            할인 & 행사
-                        </li>
-                        <li>
-                            <span>
-                                <img src="./images/benefit_img_03.svg" alt="benefit" />
-                            </span>
-                            VIP 세일 & <br />
-                            프라이빗 이벤트
-                        </li>
-                    </ul>
-                    <Link to="/crocsclub">CrocsClub Join</Link>
-                </div> */}
                 <CrocsClubJion />
             </div>
         </div>
