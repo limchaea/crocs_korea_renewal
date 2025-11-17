f80f1029e8040b927a5bc68f2bfdbf101535d806
Lim Chae A <imchae4@gmail.com>
2025-11-17 18:07:00 +0900

    메인 페이지 팝업 버튼 수정

--- src/components/ComeAsPopupBtn.jsx
+++ src/components/ComeAsPopupBtn.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './scss/comeAsBtn.scss';
 
 const ComeAsPopupBtn = () => {
     return (
-        <div className="come_as_btn">
-            <button>Come As You Are</button>
-            <Link to="/comaspopup">Come As You Are</Link>
-        </div>
+        <>
+            <div className="come_as_btn">
+                <button>Come As You Are</button>
+                {/* <Link to="/comaspopup">Come As You Are</Link> */}
+            </div>
+            <div className="come_as_btn">
+                <Link to="/comaspopup">Come As You Are</Link>
+            </div>
+        </>
     );
 };
 
