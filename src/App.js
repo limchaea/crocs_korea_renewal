ae3dd3a5ce8375daed824531134e287875669eac
wow-woody <jjeongwuss1@gmail.com>
2025-11-25 19:52:28 +0900

    2025-11-25(화) 지인 - v01

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Main from './pages/Main';
 // import New from './pages/New';
 // import Women from './pages/Women';
@@ -15,13 +15,16 @@ import CrocsClubPopup from './components/CrocsClubPopup';
 import UserInfo from './pages/UserInfo';
 import Nonmember from './pages/Nonmember';
 import ComeAsPopup from './components/ComeAsPopup';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { loginAuthStore } from './store/loginStore';
 import JibbitzCollaboProductDetail from './pages/JibbitzCollaboProductDetail';
 // import Order from './components/Order/Order';
 import WishList from './pages/WishList';
 import OrderHistory from './pages/OrderHistory';
 import Cart from './pages/Cart';
+import CartSidebar from "./components/CartSidebar";
+// import RecentProducts from "./pages/RecentProducts";
+import RecentSidebar from "./components/RecentSidebar";
 import JibbitzProductDetail from './pages/JibbitzProductDetail';
 import JibbitzProductListPage from './pages/JibbitzProductListPage';
 import ProductListPage from './pages/ProductListPage';
@@ -32,7 +35,16 @@ import Promotion from './pages/Promotion';
 import StoreLocator from './components/StoreLocator';
 
 function App() {
+    const [isCartOpen, setIsCartOpen] = useState(false);
+    const [isRecentOpen, setIsRecentOpen] = useState(false);
+    const location = useLocation();
     const { user, loading, checkSession, initAuthListener } = loginAuthStore();
+
+    // 페이지 이동 시 장바구니 닫기 + 최근본상품 닫기
+    useEffect(() => {
+        setIsCartOpen(false);
+        setIsRecentOpen(false);
+    }, [location.pathname]);
 
     // Firebase 세션 복원
     useEffect(() => {
@@ -51,7 +63,9 @@ function App() {
 
     return (
         <div className="App">
-            <Header />
+            <Header onCartClick={() => setIsCartOpen(true)} onRecentClick={() => setIsRecentOpen(true)} />
+            <CartSidebar isOpen={isCartOpen} onClose={() => setIsCartOpen(false)} />
+            <RecentSidebar isOpen={isRecentOpen} onClose={() => setIsRecentOpen(false)} />
             <Routes>
                 <Route index element={<Main />} />
                 <Route path="/jibbitz" element={<JibbitzProductListPage />} />
@@ -65,16 +79,14 @@ function App() {
                 <Route path="/crocsclub" element={<CrocsClubPopup />} />
                 <Route path="/userinfo" element={<UserInfo />} />
                 <Route path="/nonmember" element={<Nonmember />} />
-                <Route path="/comaspopup" element={<ComeAsPopup />} />
-                <Route path="/product/:id" element={<JibbitzCollaboProductDetail />} />
-                <Route path="/wishlist" element={<WishList />} />
-                <Route path="/orderhistory" element={<OrderHistory />} />
+                <Route path="/comaspopup" element={<ComeAsPopup />} />x
                 <Route path="/cart" element={<Cart />} />
+                <Route path="/wishlist" element={<WishList />} />
+                {/* <Route path='./recent' element={<RecentProducts />} /> */}
                 <Route path="/jibbitz/:cate/:id" element={<JibbitzProductListPage />} />
                 <Route path="/jibbitz/:id" element={<JibbitzProductDetail />} />
-                <Route path="/wishlist" element={<WishList />} />
+                <Route path="/product/:id" element={<JibbitzCollaboProductDetail />} />
                 <Route path="/orderhistory" element={<OrderHistory />} />
-                <Route path="/cart" element={<Cart />} />
                 <Route path="/:cate/:subcategory?" element={<ProductListPage />} />
                 {/* <Route path="/cscenter" element={<CustomerService />} /> */}
                 <Route path="/product/:id" element={<CrocsProductDetail />} />
