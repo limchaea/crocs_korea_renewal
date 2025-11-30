279368cc180deacd132ab535ee5319a6c19df6e2
jiin <amesystand@gmail.com>
2025-12-01 02:30:46 +0900

    2025-12-01(월) 지인 컬러필터 사이즈필터 전체제거버튼 추가

--- src/components/LeftNavigation.jsx
+++ src/components/LeftNavigation.jsx
@@ -1,73 +1,58 @@
-import React, { useEffect } from 'react';
-import Breadcrumbs from './Breadcrumbs';
-import SizeMenu from './SizeMenu';
-import FilterMenu from './FilterMenu';
-import ColorMenu from './ColorMenu';
-import './scss/leftNavigation.scss';
-import { useCrocsSizeStore } from '../store/useCrocsSizeStore';
-import { useParams } from 'react-router-dom';
-import { useColorFilterStore } from '../store/useColorFilterStore';
+import React, { useEffect } from "react";
+import Breadcrumbs from "./Breadcrumbs";
+import SizeMenu from "./SizeMenu";
+import FilterMenu from "./FilterMenu";
+import ColorMenu from "./ColorMenu";
+import { useCrocsSizeStore } from "../store/useCrocsSizeStore";
+import { useColorFilterStore } from "../store/useColorFilterStore";
+import { useParams } from "react-router-dom";
+import "./scss/leftNavigation.scss";
 
 export default function LeftNavigation({
-    category,
-    subcategory,
-    selectedSize,
-    onSizeSelect,
-    filters = [],
-    priceRanges = [],
-    colors = [],
+  category,
+  subcategory,
+  selectedSize,
+  onSizeSelect,
 }) {
-    const { crocsSizes, onFetchSize } = useCrocsSizeStore();
-    const { selectedColors, toggleColor } = useColorFilterStore();
-    const params = useParams();
+  const { crocsSizes, onFetchSize } = useCrocsSizeStore();
+  const { selectedColors, toggleColor } = useColorFilterStore();
+  const params = useParams();
 
-    // ⭐ 최종적으로 사용할 category, subcategory
-    const finalCategory = category || params.cate || 'new';
-    const finalSubcategory = subcategory || params.subcategory || null;
+  const finalCategory = category || params.cate || "new";
+  const finalSubcategory = subcategory || params.subcategory || null;
 
-    useEffect(() => {
-        onFetchSize();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+  useEffect(() => {
+    onFetchSize();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    // 색상 제거 핸들러
-    const handleRemoveColor = (colorToRemove) => {
-        toggleColor(colorToRemove);
-    };
+  const handleRemoveColor = (color) => toggleColor(color);
+  const handleRemoveSize = () => onSizeSelect?.(null);
 
-    // 사이즈 제거 핸들러
-    const handleRemoveSize = () => {
-        onSizeSelect?.(null);
-    };
+  return (
+    <div className="left_nav__section_wrap">
+      <Breadcrumbs category={finalCategory} subcategory={finalSubcategory} />
+      <div className="left_nav">
+        <FilterMenu
+          selectedColors={selectedColors}
+          selectedSize={selectedSize}
+          onRemoveColor={handleRemoveColor}
+          onRemoveSize={handleRemoveSize}
+        />
 
-    return (
-        <div className="left_nav__section_wrap">
-            <Breadcrumbs category={finalCategory} subcategory={finalSubcategory} />
-            <div className="left_nav">
-                {/* ⭐ 선택된 필터들을 FilterMenu에 전달 */}
-                <FilterMenu
-                    selectedColors={selectedColors}
-                    selectedSize={selectedSize}
-                    onRemoveColor={handleRemoveColor}
-                    onRemoveSize={handleRemoveSize}
-                />
+        <div className="breadcrumbs__line" />
 
-                <div className="breadcrumbs__line" />
+        <SizeMenu
+          sizes={crocsSizes}
+          selectedSize={selectedSize}
+          onSizeSelect={onSizeSelect}
+        />
 
-                <SizeMenu
-                    sizes={crocsSizes}
-                    selectedSize={selectedSize}
-                    onSizeSelect={onSizeSelect}
-                />
+        <div className="breadcrumbs__line" />
 
-                <div className="breadcrumbs__line" />
-
-                <ColorMenu />
-
-                <div className="breadcrumbs__line" />
-
-                {/* <PriceMenu priceRanges={priceRanges} /> */}
-            </div>
-        </div>
-    );
+        <ColorMenu />
+        <div className="breadcrumbs__line" />
+      </div>
+    </div>
+  );
 }
