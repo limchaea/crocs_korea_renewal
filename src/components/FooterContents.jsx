67bb3564d00456418a07b79763327edb56c5c5c3
chowon <zzzeus9876@gmail.com>
2025-12-01 10:04:23 +0900

    2025-12-01(월) 푸터 sns 수정

--- src/components/FooterContents.jsx
+++ src/components/FooterContents.jsx
@@ -18,16 +18,16 @@ const FooterContents = () => {
             <div className="footer_right">
                 <div className="sns_img_box">
                     <p>
-                        <img src="/images/icon-insta.png" alt="" />
+                        <img src="/images/icon-insta.png" alt="icon-insta" />
                     </p>
                     <p>
-                        <img src="/images/icon-youtube.png" alt="" />
+                        <img src="/images/icon-youtube.png" alt="icon-youtube" />
                     </p>
                     <p>
-                        <img src="/images/icon-kakao.png" alt="" />
+                        <img src="/images/icon-kakao.png" alt="icon-kakao" />
                     </p>
                     <p>
-                        <img src="/images/icon-tiktok.png" alt="" />
+                        <img src="/images/icon-tiktok.png" alt="icon-tiktok" />
                     </p>
                 </div>
                 <div className="copy-text">© 2025 Crocs Retail, LLC</div>
